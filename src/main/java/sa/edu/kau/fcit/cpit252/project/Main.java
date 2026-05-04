019ac354481743131b90e61079dd33e88c0e8c1c
Motaz <vlxea88@gmail.com>
2026-05-04 19:29:36 +0300

    Quantitative View Limits #18 And Download Restrictions #16 Closed

--- src/main/java/sa/edu/kau/fcit/cpit252/project/Main.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("=== Secure File Transfer System (SFTS) Initialized ===\n");
 
         // 1. Using Builder Pattern to create a new medical file
+        // 1. Using Builder Pattern
         System.out.println("--- 1. Building Secure File ---");
         SecureFile patientFile = new FileBuilder()
                 .setId("PT-9982")
@@ -16,6 +17,8 @@ public class Main {
                 .setDept("Laboratory")
                 .setEncryption("AES-256")
                 .setUrgent(true)
+                .setMaxViews(2) // خليناها 2 عشان نجرب الحد
+                .setViewOnly(true) // نفعل خاصية منع التحميل
                 .build();
         System.out.println("System Log: " + patientFile.toString() + " created successfully.\n");
 
@@ -27,13 +30,23 @@ public class Main {
         // 3. Using Proxy Pattern (and Singleton Logger internally) for Authorized Access
         System.out.println("--- 3. Attempting Authorized Access ---");
         Department authorizedAccess = new DepartmentProxy(labDepartment, "Abdulaziz_Bukhari");
-        authorizedAccess.processFile("MRI_Scan_Results.pdf");
+
+        // التجربة الأولى (بتنجح ويقولك للعرض فقط)
+        authorizedAccess.processFile(patientFile);
         System.out.println();
 
-        // 4. Using Proxy Pattern for Unauthorized Access
+        // التجربة الثانية (بتنجح)
+        authorizedAccess.processFile(patientFile);
+        System.out.println();
+
+        // التجربة الثالثة (بترفض لأن الحد الأقصى 2)
+        System.out.println("--- Attempting 3rd Access (Should Fail) ---");
+        authorizedAccess.processFile(patientFile);
+        System.out.println();
+
         System.out.println("--- 4. Attempting Unauthorized Access ---");
         Department unauthorizedAccess = new DepartmentProxy(labDepartment, "Motaz_Alsayed");
-        unauthorizedAccess.processFile("MRI_Scan_Results.pdf");
+        unauthorizedAccess.processFile(patientFile);
 
         System.out.println("\n=== System Shutting Down ===");
     }
